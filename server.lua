c4913143d578cabc156837f4b0fa311f8b7e373c
Sampuke <99171091+Sampuke@users.noreply.github.com>
2023-05-22 09:07:51 +0300

    Esx / qb support

--- server.lua
+++ server.lua
@@ -1,8 +1,54 @@
-local QBCore
+local Core, CoreName
 if GetResourceState('qb-core') == 'started' then
     QBCore = exports['qb-core']:GetCoreObject()
+    CoreName = 'qb'
+elseif
+    Core = exports["es_extended"]:getSharedObject()
+    CoreName = 'esx'
 else
-    warn('qb-core is missing, modifications won\'t cost anything')
+    print('Core is missing nothing will cost')
+end
+Functions = {}
+
+Functions.qb = {}
+Functions.qb.GetPlayer = function(src)
+    return Core.Functions.GetPlayer(src)
+end
+Functions.qb.Getmoney = function(player, type)
+    return player.Functions.GetMoney(type)
+end
+Functions.qb.RemoveMoney = function(player, type, amount)
+    return player.Functions.RemoveMoney(type, amount, "Customs")
+end
+Functions.qb.GetJob = function(player)
+    return player.PlayerData.job.name 
+end
+Functions.qb.Owned = function(vehicleplate)
+    local result = MySQL.salar.await('SELECT 1 from player_vehicles WHERE plate = ?', {plate})
+    return result
+end
+Functions.esx.Update = function(vehicleplate)
+    MySQL.update('UPDATE player_vehicles SET mods = ? WHERE plate = ?', {json.encode(vehicleProps), vehicleProps.plate})
+end
+Functions.esx = {}
+Functions.esx.GetPlayer = function(src)
+   return Core.GetPlayerFromId(src) 
+end
+Functions.esx.Getmoney = function(player, type)
+    return player.getAccount(type).money
+end
+Functions.esx.RemoveMoney = function(player, type, amount)
+    return player.removeAccountMoney(type, amount)
+end
+Functions.esx.GetJob = function(player)
+    return player.getJob().name
+end
+Functions.esx.Owned = function(vehicleplate)
+    local result = MySQL.salar.await('SELECT 1 from owned_vehicles WHERE plate = ?', {plate})
+    return result
+end
+Functions.esx.Update = function(vehicleplate)
+    MySQL.update('UPDATE owned_vehicles SET vehicle = ? WHERE plate = ?', {json.encode(vehicleProps), vehicleProps.plate})
 end
 
 ---@return number
@@ -18,16 +64,16 @@ end
 ---@param amount number
 ---@return boolean
 local function removeMoney(source, amount)
-    if not QBCore then return true end
-    local player = QBCore.Functions.GetPlayer(source)
-    local cashBalance = player.Functions.GetMoney('cash')
-    local bankBalance = player.Functions.GetMoney('bank')
+    if not CoreName then return true end
+    local player = Functions[CoreName].GetPlayer(source)
+    local cashBalance = Functions[CoreName].GetMoney(player, 'cash')
+    local bankBalance = Functions[CoreName].GetMoney(player, 'bank')
 
     if cashBalance >= amount then
-        player.Functions.RemoveMoney('cash', amount, "Customs")
+        Functions[CoreName].RemoveMoney(player, 'cash', amount)
         return true
     elseif bankBalance >= amount then
-        player.Functions.RemoveMoney('bank', amount, "Customs")
+        Functions[CoreName].RemoveMoney(player, 'bank', amount)
         lib.notify(source, {
             title = 'Customs',
             description = ('You paid $%s from your bank account'):format(amount),
@@ -45,7 +91,8 @@ lib.callback.register('customs:server:pay', function(source, mod, level)
 
     for i, v in ipairs(Config.Zones) do
         if i == zone and v.freeMods then
-            local playerJob = QBCore.Functions.GetPlayer(source)?.PlayerData?.job?.name
+            local player = Functions[CoreName].GetPlayer(source)
+            local playerJob = Functions[CoreName].GetJob(player)
             for _, job in ipairs(v.freeMods) do
                 if playerJob == job then
                     return true
@@ -63,7 +110,8 @@ lib.callback.register('customs:server:repair', function(source, bodyHealth)
 
     for i, v in ipairs(Config.Zones) do
         if i == zone and v.freeRepair then
-            local playerJob = QBCore.Functions.GetPlayer(source)?.PlayerData?.job?.name
+            local player = Functions[CoreName].GetPlayer(source)
+            local playerJob = Functions[CoreName].GetJob(player)
             for _, job in ipairs(v.freeRepair) do
                 if playerJob == job then
                     return true
@@ -76,20 +124,12 @@ lib.callback.register('customs:server:repair', function(source, bodyHealth)
     return removeMoney(source, price)
 end)
 
-local function IsVehicleOwned(plate)
-    local result = MySQL.scalar.await('SELECT 1 from player_vehicles WHERE plate = ?', {plate})
-    if result then
-        return true
-    else
-        return false
-    end
-end
 
 --Copied from qb-mechanicjob
 RegisterNetEvent('customs:server:saveVehicleProps', function()
     local src = source --[[@as number]]
     local vehicleProps = lib.callback.await('customs:client:vehicle', src)
-    if IsVehicleOwned(vehicleProps.plate) then
-        MySQL.update('UPDATE player_vehicles SET mods = ? WHERE plate = ?', {json.encode(vehicleProps), vehicleProps.plate})
+    if Functions[CoreName].Owned(vehicleProps.plate) then
+        Fuctions[CoreName].Update(vehicleprops)
     end
 end)
