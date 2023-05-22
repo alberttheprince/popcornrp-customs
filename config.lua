b2eda829fe7142565e8b3abd1be1b8701409ec8e
Sampuke <99171091+Sampuke@users.noreply.github.com>
2023-05-22 09:20:20 +0300

    Added performance upgrade config

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Currency = '$'
-
+Config.Performance = true -- This enables performance upgrades
 ---@type ZoneOptions[]
 Config.Zones = {
 -- These MUST be configured with your own location before using them. The current coordinates are set for MOLO MRPD and Benny's
